ccc17e5e4e46fcfa044620c20fe0422c63c98005
Hradis Michal <ihradis@fit.vutbr.cz>
2014-07-07 15:03:23 +0200

    Moved all scripts to single directory. Added scripts for creating caffe configurations.

---
+++ scripts/configure_net/buildAll.sh
@@ -0,0 +1,26 @@
+# Creates configuration files for caffe
+
+DEF_DIR=./definitions/
+
+for DEF in `ls "$DEF_DIR"`
+do
+    NET_DIR="./NETS/$DEF"
+    mkdir -p "$NET_DIR"
+
+    BODY=`./prepareNet.sh <"$DEF_DIR/$DEF"`
+    LAST_TOP=`echo "$BODY" | sed -e 's/^[ \t]*//' | grep top: |tail -n 1| cut -f 2 -d \ `
+
+    (
+        cat train_start
+        echo "$BODY"
+        cat train_end | sed "s/bottom:$/bottom: $LAST_TOP/"
+    ) > "$NET_DIR/net_train.prototxt"
+    (
+        cat val_start
+        echo "$BODY"
+        cat val_end | sed "s/bottom:$/bottom: $LAST_TOP/"
+    ) > "$NET_DIR/net_val.prototxt"
+
+    cp net_solver.prototxt "$NET_DIR"
+
+done
